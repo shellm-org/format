6710f9e0540b2278980e5db4af9922e28add636b
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2018-07-02 20:00:44 +0200

    Update shellman location and deps

--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
 BINS="bin/format"  #:bin/other-script
 LIBS="lib/format.sh"
-DEPS=shellm/shellm-shellman  #:user2/repo2
+DEPS=gitlab.com/shellm/shellman  #:user2/repo2
 BASH_COMPLETIONS="cmp/format.completion.bash"
 ZSH_COMPLETIONS="cmp/format.completion.zsh"
